e6f0eb5d0f57fc9151de913d996b1cd2215215b1
Abhishek M <abhishekm@wecm-9-67-61-200.wecm.ibm.com>
2024-07-10 14:33:53 -0400

    turn on logging and torch compile
    Signed-off-by: Abhishek M <abhishekm@wecm-9-67-167-148.wecm.ibm.com>

--- tuning/sft_trainer.py
+++ tuning/sft_trainer.py
@@ -320,8 +320,8 @@ def train(
 
     #########TOTCH.COMPILE##############
     train_args.torch_compile = True
-    # train_args.torch_compile_backend = "inductor"
-    # train_args.torch_compile_mode = "default"
+    train_args.torch_compile_backend = "inductor"
+    train_args.torch_compile_mode = "default"
     train_args.include_tokens_per_second = True
 
     # print("train_args: %s", train_args)
