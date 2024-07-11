a5a26b79f7b9d1996329df163a8f96e362658ae0
Abhishek M <abhishekm@wecm-9-67-164-109.wecm.ibm.com>
2024-07-11 14:07:43 -0400

    removing all train args from code
    Signed-off-by: Abhishek M <abhishekm@wecm-9-67-167-148.wecm.ibm.com>

--- tuning/sft_trainer.py
+++ tuning/sft_trainer.py
@@ -319,11 +319,11 @@ def train(
         )
 
     #########TOTCH.COMPILE##############
-    train_args.torch_compile = True
-    train_args.torch_compile_backend = "inductor"
-    train_args.torch_compile_mode = "default"
-    train_args.include_tokens_per_second = True
-    train_args.save_total_limit = 3
+    # train_args.torch_compile = True
+    # train_args.torch_compile_backend = "inductor"
+    # train_args.torch_compile_mode = "default"
+    # train_args.include_tokens_per_second = True
+    # train_args.save_total_limit = 3
 
     # print("train_args: %s", train_args)
     logger.debug("train_args: %s", train_args)
